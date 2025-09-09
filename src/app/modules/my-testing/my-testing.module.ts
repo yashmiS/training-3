1345211709c2b75613070aa007be08822fa769d9
yashmiS <savindiabeywickrama@gmail.com>
2025-09-09 14:16:26 +0530

    Configure my testing module

---
+++ src/app/modules/my-testing/my-testing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyTestingRoutingModule } from './my-testing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyTestingRoutingModule
+  ]
+})
+export class MyTestingModule { }
